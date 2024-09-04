dd380ade7a561f1436d118012a4eccad2b8b8221
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-09-04 23:21:25 +0500

    edited height

---
+++ src/pages/layout.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserAddOutlined,
+  LoginOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu, theme } from "antd";
+const { Header, Sider, Content } = Layout;
+
+const Layer = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <>
+      <Layout style={{ height: "100vh" }}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={[
+              {
+                key: "1",
+                icon: <HomeOutlined />,
+                label: <Link to="/">Home</Link>,
+              },
+              {
+                key: "2",
+                icon: <LoginOutlined />,
+                label: <Link to="login">Log in</Link>,
+              },
+              {
+                key: "3",
+                icon: <UserAddOutlined />,
+                label: <Link to="signin">Sign in</Link>,
+              },
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          >
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
+};
+
+export default Layer;
