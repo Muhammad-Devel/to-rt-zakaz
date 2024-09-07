4361fa88d16433cc35908cb617cfb4ccc9d43c4a
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-09-07 12:37:38 +0500

    products edited

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -46,11 +46,10 @@ export default function Products() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-6 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
-        {/* <h2 className="sr-only text-gray-700">Products</h2> */}
-
-        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-6">
+        
+        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-6 ">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
+            <a key={product.id} href={product.href} className="group border-transparent border-solid border rounded-lg shadow-md shadow-gray-200">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img
                   alt={product.imageAlt}
