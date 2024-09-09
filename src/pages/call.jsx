231157e9831d1474f48fd9c0c592cf61f2b35274
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-09-09 11:57:50 +0500

    edited callbanner

--- src/pages/call.jsx
+++ src/pages/call.jsx
@@ -1,10 +1,21 @@
 import { Card } from 'antd';
+import { PhoneOutlined } from '@ant-design/icons'
 
 const Call = () => {
     return (
-        <Card>
-    <div className='w-full'>call banner</div>
-  </Card>
+        <>
+            <Card style={{
+                marginTop: 10,
+                marginLeft: 10,
+                marginRight: 10,
+            }}>
+                <div className='w-full text-2xl'>
+                    Call banner
+                    <button className='float-right' type='success'><PhoneOutlined /></button>
+                </div>
+               
+            </Card>
+        </>
     )
-  };
-  export default Call;
+};
+export default Call;
