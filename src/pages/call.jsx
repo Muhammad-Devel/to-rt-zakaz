2e1157b38ea9547552075bace904fb1afdff8327
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-09-11 14:10:52 +0500

    text size

--- src/pages/call.jsx
+++ src/pages/call.jsx
@@ -10,12 +10,12 @@ const Call = () => {
                 marginRight: 10,
             }}>
 
-                <div className='w-full text-2xl flex justify-between'>
-                    <div>
+                <div className='w-full flex justify-between'>
+                    <div className='text-4xl md:text-3xl sm:text-2xl'>
                         Use Call
                     </div>
                     <div>
-                        <span className='mr-6'>
+                        <span className='text-4xl md:text-3xl sm:text-2xl mr-6'>
                             +998 (91) 765-43-21
                         </span>
                         <button className='bg-green-500 rounded-full width min-w-28 text-white py-1' type='success'><PhoneOutlined /></button>
