05fcc662bc708c729a580b3b3529cd2c851048b4
Muhammad-Devel <m.jumaboyev@mail.ru>
2024-09-11 15:19:06 +0500

    call banner edited

--- src/pages/call.jsx
+++ src/pages/call.jsx
@@ -11,14 +11,14 @@ const Call = () => {
             }}>
 
                 <div className='w-full flex justify-between'>
-                    <div className='text-4xl md:text-3xl sm:text-2xl'>
+                    <div className='text-2xl md:text-xl sm:text-lg'>
                         Use Call
                     </div>
                     <div>
-                        <span className='text-4xl md:text-3xl sm:text-2xl mr-6'>
+                        <span className='text-2xl md:text-xl sm:text-lg mr-6'>
                             +998 (91) 765-43-21
                         </span>
-                        <button className='bg-green-500 rounded-full width min-w-28 text-white py-1' type='success'><PhoneOutlined /></button>
+                        <button className='bg-green-500 rounded-full width min-w-28 text-white py-1 text-2xl md:text-xl sm:text-lg' type='success'><PhoneOutlined /></button>
                     </div>
                 </div>
 
